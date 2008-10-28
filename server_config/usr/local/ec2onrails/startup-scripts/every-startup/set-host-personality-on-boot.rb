99110dd3be2bf7c50605d109ce981287baf12cb7
Bill <bill.burcham@gmail.com>
2008-10-28 08:37:35 -0700

    Set up libstdc++.so link for ImageScience to work around bug http://rubyforge.org/tracker/?group_id=1513&atid=5921&func=detail&aid=22585

--- server_config/usr/local/ec2onrails/startup-scripts/every-startup/set-host-personality-on-boot.rb
+++ server_config/usr/local/ec2onrails/startup-scripts/every-startup/set-host-personality-on-boot.rb
@@ -3,6 +3,10 @@ $:.unshift(File.expand_path(File.join( File.dirname(__FILE__), '../../lib/')))
 require 'ec2-instance-data'
 require 'register-ddns'
 
+# ImageScience bug http://rubyforge.org/tracker/?group_id=1513&atid=5921&func=detail&aid=22585
+# needs this link
+`[ -e /usr/lib/libstdc++.so ] || ln -s /usr/lib/libstdc++.so.6 /usr/lib/libstdc++.so`
+
 # always set hostname (if it's available)
 `sudo hostname "#{EC2InstanceData.user_data['public-hostname']}"` if  EC2InstanceData.user_data.has_key? 'public-hostname'
 
